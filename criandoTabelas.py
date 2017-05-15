9951aee88447e1349be617f3ef4c845f87e2e9d6
Marcos Aurélio <coaleth@gmail.com>
2017-05-15 18:11:46 -0300

    atualizacao

---
+++ criandoTabelas.py
@@ -0,0 +1,10 @@
+from criacaoBanco import *
+# Criando tabela albuns
+
+def criaTabelaAlbuns():
+    cursor.execute("""CREATE TABLE albuns
+                    (title text,
+                    artist text,
+                    release_date text,
+                    publisher text,
+                    media_type text""")
